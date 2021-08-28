1527e8c7686344facb842cc156528c02b1bd3af6
otoalasania <otar.alasania@gmail.com>
2021-08-28 16:08:24 +0400

    loader
    i added preloader function with logo image

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ function scrollToTop(){
     })
 }
 //SCROLL UP END//
+
+//LOADER START//
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; 
+});
+//LOADER END//
